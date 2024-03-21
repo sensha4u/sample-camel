9c5588656df1c30abfe22297b9a2026f7ae5192c
sensha4u <sensha4u@gmail.com>
2024-03-21 13:05:48 -0500

    Update SampleCamelApplicationTests.java

--- src/test/java/com/sensha4u/springboot/camel/SampleCamelApplicationTests.java
+++ src/test/java/com/sensha4u/springboot/camel/SampleCamelApplicationTests.java
@@ -1,7 +1,12 @@
 package com.sensha4u.springboot.camel;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest()
 class SampleCamelApplicationTests {
